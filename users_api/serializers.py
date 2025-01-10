858803a6f8e91b8dd2e2b5c62ced457aca25178e
C0R4L0V3 <coraline.e.love@gmail.com>
2025-01-10 16:57:38 -0600

    fixed registration error

--- users_api/serializers.py
+++ users_api/serializers.py
@@ -8,14 +8,14 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['username', 'password', 'email', 'first_name', 'last_name']
         
-        def create(self, validated_data):
-        #Create a user instance while hashing the password
-            user = User.objects.create_user(
-                username=validated_data['username'],
-                password=validated_data['password'],
-                email=validated_data.get('email', ''),
-                first_name=validated_data.get('first_name', ''),
-                last_name=validated_data.get('last_name', '')
-            )
+    def create(self, validated_data):
+    #Create a user instance while hashing the password
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data.get('email', ''),
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
+        )
 
-            return user
+        return user
